20cdbeb58240f4ae14236ce133ce70fe253d7562
Victory Iyakoregha <victoryiyakoregha@Victorys-MacBook-Pro.local>
2025-08-02 16:42:56 -0500

    init commit

---
+++ DeepDiveRecommender.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SpotifyWebApp;
+
+public class DeepDiveRecommender
+{
+    private readonly ILogger<DeepDiveRecommender> _logger;
+
+    public DeepDiveRecommender(ILogger<DeepDiveRecommender> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("DeepDiveRecommender")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
