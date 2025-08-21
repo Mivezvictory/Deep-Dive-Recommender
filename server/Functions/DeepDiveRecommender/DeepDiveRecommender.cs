e57618d35fbddf75187040a09f13ccf925d07bd9
Victory Iyakoregha <victoryiyakoregha@Victorys-MacBook-Pro.local>
2025-08-21 11:05:12 -0500

    Moved functions into a different folder structure: Moved SpotifyCallback, SpotifyAuth, and DeepDiveRecommender folders into the Functions folder. This is being done to give the project(function app) a better file structure

--- server/Functions/DeepDiveRecommender/DeepDiveRecommender.cs
+++ server/Functions/DeepDiveRecommender/DeepDiveRecommender.cs
@@ -15,7 +15,7 @@ public class DeepDiveRecommender
     }
 
     [Function("DeepDiveRecommender")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
